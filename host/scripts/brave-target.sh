df09b7e4215536b6e27a5b32cfecf82c22275f4e
Francesco Minnocci <ad17fmin@uwcad.it>
2020-06-24 02:06:52 +0200

    add brave target script

---
+++ host/scripts/brave-target.sh
@@ -0,0 +1,8 @@
+# Environment variables
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
+
+PLATFORM='chrome'
+
+# Native messaging host location
+# https://developer.chrome.com/extensions/nativeMessaging#native-messaging-host-location
+    TARGET="$XDG_CONFIG_HOME/BraveSoftware/Brave-Browser/NativeMessagingHosts"
